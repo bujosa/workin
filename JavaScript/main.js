a98adfac2883f8974e52aab2bbf23541540e9e17
Bant89 <bant8009@gmail.com>
2019-10-01 10:59:58 -0400

    Added the login

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,7 +3,34 @@ const modal = document.querySelector("#my-modal");
 const modalBtn = document.querySelector("#modal-btn");
 const closeBtn = document.querySelector(".close");
 const saveBtn = document.querySelector("#saveForm");
-let horaIncidente, categoriaIncidente, fechaIncidente, contentString;
+let horaIncidente,
+  categoriaIncidente,
+  fechaIncidente,
+  contentString,
+  descripcion,
+  modoDelincuente;
+
+let postLocationData = async (datos = {}) => {
+  response = await fetch("http://localhost:8080/LocationAdd", {
+    method: "POST",
+    mode: "no-cors",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(datos)
+  });
+};
+
+let postCrimeData = async (datos = {}) => {
+  response = await fetch("http://localhost:8080/CrimeAdd", {
+    method: "POST",
+    mode: "no-cors",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(datos)
+  });
+};
 
 //Hide all markers
 let hideAllInfoWindows = map => {
@@ -42,20 +69,40 @@ saveBtn.addEventListener("click", e => {
   ).filter(e => e.checked)[0].value;
   horaIncidente = document.querySelector('[type="time"]').value;
   fechaIncidente = document.querySelector('[type="date"]').value;
+  descripcion = document.getElementById("textoDescripcion").value;
+  // modoDelincuente = Array.from(
+  //   document.querySelectorAll('[name="modo"]')
+  // ).filter(e => e.checked)[0].value;
+  modoDelincuente = "pasola";
   contentString = `
   <b>Hora del incidente: </b> ${horaIncidente} <br />
   <b>Categoria de incidente: </b> ${categoriaIncidente} <br />
-  <b>Fecha del incidente: </b> ${fechaIncidente}
+  <b>Fecha del incidente: </b> ${fechaIncidente} <br />
+  <b>Descripcion: </br> ${descripcion}
   `;
+  let crimeData = {
+    Id: 1,
+    Loc: 123.354,
+    Cat: categoriaIncidente,
+    Date: fechaIncidente,
+    Time: horaIncidente,
+    Description: descripcion,
+    Mode: modoDelincuente
+  };
+
   closeModal();
   var infowindow = new google.maps.InfoWindow({
     content: contentString
   });
 
   map.addListener("click", function(e) {
-    console.log(`Latitud: ${e.latLng.lat()}, Longitud: ${e.latLng.lng()}`);
+    //console.log(`Latitud: ${e.latLng.lat()}, Longitud: ${e.latLng.lng()}`);
+    postLocationData({
+      User: 2,
+      Latitude: e.latLng.lat(),
+      Longitude: e.latLng.lng()
+    });
     let res = placeMarker(e.latLng, map, infowindow);
-    debugger;
     google.maps.event.addListener(res, "click", function() {
       hideAllInfoWindows(map);
       this.infowindow.open(map, this);
@@ -64,13 +111,14 @@ saveBtn.addEventListener("click", e => {
     google.maps.event.clearInstanceListeners(map);
 
     let entradas = Array.from(document.querySelectorAll("input"));
-
+    debugger;
     entradas.forEach(i => {
-      if (entradas[i].type === "radio") entradas[i].checked = false;
+      if (i.type === "radio") i.checked = false;
       else {
-        if (entradas[i].type !== "submit") entradas[i].value = "";
+        if (i.type !== "submit") i.value = "";
       }
     });
     document.querySelector("textarea").value = "";
+    postCrimeData(crimeData);
   });
 });
