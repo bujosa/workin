31e5ec1b78ed59996ed35b92be3d137ca30b064e
erica ross <ergerica76@gmail.com>
2019-10-01 10:45:28 -0400

    more users
    pueden haber duplicates

---
+++ DataBases [EnZonaRD]/more users.sql
@@ -0,0 +1,108 @@
+INSERT [dbo].[Usuarios] ([Nombre], [Apellido], [Contrasena], [Email], [Telefono], [Genero], [Direccion]) 
+VALUES (N'rasnuel', N'saud', N'zVHDfXZjE', N'fdfds@gmail.com', N'8295889032', N'M', 'Calle luperon 85 ')
+
+INSERT [dbo].[Delitos] ([IdCategoria], [Id_Locacion], [Fecha], [Hora], [Descripcion], [Modo]) 
+VALUES (15,15, CAST(N'2019-08-25' AS Date), CAST(N'13:26:47.8170000' AS Time), N'Me robaron ', N'Motor')
+
+INSERT [dbo].[Locaciones] ([IdUsuario], [Longitud], [Latitud]) 
+VALUES (15, CAST(18.493541000 AS Decimal(9, 6)), CAST(-69.878562000 AS Decimal(9, 6)))
+
+
+
+INSERT [dbo].[Usuarios] ([Nombre], [Apellido], [Contrasena], [Email], [Telefono], [Genero], [Direccion]) 
+VALUES (N'inuela', N'Nomare', N'mAXzb98U', N'afjbkdjfe4@gmail.com', N'8296792431', N'F', 'Calle marlon 54')
+
+INSERT [dbo].[Delitos] ([IdCategoria], [Id_Locacion], [Fecha], [Hora], [Descripcion], [Modo]) 
+VALUES (16,16, CAST(N'2019-09-25' AS Date), CAST(N'04:45:52.8170000' AS Time), N'No hay cellphone', N'Pasola')
+
+INSERT [dbo].[Locaciones] ([IdUsuario], [Longitud], [Latitud]) 
+VALUES (16, CAST(18.4885241000 AS Decimal(9, 6)), CAST(-69.884562000 AS Decimal(9, 6)))
+
+
+
+INSERT [dbo].[Usuarios] ([Nombre], [Apellido], [Contrasena], [Email], [Telefono], [Genero], [Direccion]) 
+VALUES (N'Francel', N'Agustin', N'ucdtUsCUe4', N'ek;awr8k65@gmail.com', N'8292979038', N'M', 'Calle muy 45')
+
+INSERT [dbo].[Delitos] ([IdCategoria], [Id_Locacion], [Fecha], [Hora], [Descripcion], [Modo]) 
+VALUES (17,17, CAST(N'2019-06-02' AS Date), CAST(N'18:56:52.8170000' AS Time), N'se llevaron el cell', N'Carro Publico')
+
+INSERT [dbo].[Locaciones] ([IdUsuario], [Longitud], [Latitud]) 
+VALUES (17, CAST(18.493743000 AS Decimal(9, 6)), CAST(-69.941913000 AS Decimal(9, 6)))
+
+
+
+INSERT [dbo].[Usuarios] ([Nombre], [Apellido], [Contrasena], [Email], [Telefono], [Genero], [Direccion]) 
+VALUES (N'anuel', N'maiseno', N'8zXwZZqj', N'fkssdflj54@gmail.com', N'8097778294', N'M', 'Calle Mejia 22')
+
+INSERT [dbo].[Delitos] ([IdCategoria], [Id_Locacion], [Fecha], [Hora], [Descripcion], [Modo]) 
+VALUES (18,18, CAST(N'2019-09-23' AS Date), CAST(N'22:54:48.8170000' AS Time), N'Me robaron el celular', N'Motor')
+
+INSERT [dbo].[Locaciones] ([IdUsuario], [Longitud], [Latitud]) 
+VALUES (18, CAST(18.495625000 AS Decimal(9, 6)), CAST(-69.977806000 AS Decimal(9, 6)))
+
+
+
+INSERT [dbo].[Usuarios] ([Nombre], [Apellido], [Contrasena], [Email], [Telefono], [Genero], [Direccion]) 
+VALUES (N'London', N'Tipton', N'gSzax6GG', N'frdkwekr43@gmail.com', N'829345876', N'F', 'Calle Luo giu')
+
+INSERT [dbo].[Delitos] ([IdCategoria], [Id_Locacion], [Fecha], [Hora], [Descripcion], [Modo]) 
+VALUES (18,18, CAST(N'2019-07-13' AS Date), CAST(N'18:56:15.8170000' AS Time), N'Un hombre se llevo mi cartera', N'Persona')
+
+INSERT [dbo].[Locaciones] ([IdUsuario], [Longitud], [Latitud]) 
+VALUES (18, CAST(18.496536000 AS Decimal(9, 6)), CAST( -69.967537000 AS Decimal(9, 6)))
+
+
+
+INSERT [dbo].[Usuarios] ([Nombre], [Apellido], [Contrasena], [Email], [Telefono], [Genero], [Direccion]) 
+VALUES (N'Onil', N'Peryra', N'zVHDfXZjE', N'fdfsd@gmail.com', N'8295989132', N'M', 'Calle luperon 85 ')
+
+INSERT [dbo].[Delitos] ([IdCategoria], [Id_Locacion], [Fecha], [Hora], [Descripcion], [Modo]) 
+VALUES (15,15, CAST(N'2019-08-25' AS Date), CAST(N'13:26:47.8170000' AS Time), N'Me robaron ', N'Motor')
+
+INSERT [dbo].[Locaciones] ([IdUsuario], [Longitud], [Latitud]) 
+VALUES (15, CAST(18.493441000 AS Decimal(9, 6)), CAST(-69.878562000 AS Decimal(9, 6)))
+
+
+
+INSERT [dbo].[Usuarios] ([Nombre], [Apellido], [Contrasena], [Email], [Telefono], [Genero], [Direccion]) 
+VALUES (N'Consuelo', N'ramos', N'mAXzb98U', N'afjdkdjfe4@gmail.com', N'8296792431', N'F', 'Calle marlon 54')
+
+INSERT [dbo].[Delitos] ([IdCategoria], [Id_Locacion], [Fecha], [Hora], [Descripcion], [Modo]) 
+VALUES (16,16, CAST(N'2019-09-25' AS Date), CAST(N'04:45:52.8170000' AS Time), N'No hay mas carro se lo llevaron', N'Pasola')
+
+INSERT [dbo].[Locaciones] ([IdUsuario], [Longitud], [Latitud]) 
+VALUES (16, CAST(18.4895241000 AS Decimal(9, 6)), CAST(-69.884562000 AS Decimal(9, 6)))
+
+
+
+INSERT [dbo].[Usuarios] ([Nombre], [Apellido], [Contrasena], [Email], [Telefono], [Genero], [Direccion]) 
+VALUES (N'liselot', N'sustinto', N'ucdtUsCUe4', N'ekadwrc8k65@gmail.com', N'8292879149', N'M', 'Calle martin 45')
+
+INSERT [dbo].[Delitos] ([IdCategoria], [Id_Locacion], [Fecha], [Hora], [Descripcion], [Modo]) 
+VALUES (17,17, CAST(N'2019-06-02' AS Date), CAST(N'18:56:52.8170000' AS Time), N'se llevaron mis compras ', N'Carro Publico')
+
+INSERT [dbo].[Locaciones] ([IdUsuario], [Longitud], [Latitud]) 
+VALUES (17, CAST(18.493733000 AS Decimal(9, 6)), CAST(-69.943913000 AS Decimal(9, 6)))
+
+
+
+INSERT [dbo].[Usuarios] ([Nombre], [Apellido], [Contrasena], [Email], [Telefono], [Genero], [Direccion]) 
+VALUES (N'anuel', N'maiseno', N'8zXwZZqj', N'fkssdfdlj54@gmail.com', N'8097778294', N'M', 'Calle Mejia 32')
+
+INSERT [dbo].[Delitos] ([IdCategoria], [Id_Locacion], [Fecha], [Hora], [Descripcion], [Modo]) 
+VALUES (18,18, CAST(N'2019-09-23' AS Date), CAST(N'22:54:48.8170000' AS Time), N'Me robaron el celular', N'Motor')
+
+INSERT [dbo].[Locaciones] ([IdUsuario], [Longitud], [Latitud]) 
+VALUES (18, CAST(18.495425000 AS Decimal(9, 6)), CAST(-69.977816000 AS Decimal(9, 6)))
+
+
+
+INSERT [dbo].[Usuarios] ([Nombre], [Apellido], [Contrasena], [Email], [Telefono], [Genero], [Direccion]) 
+VALUES (N'Landon', N'Titan', N'gSzax6GG', N'frdkdwwekr43@gmail.com', N'829345876', N'M', 'Calle Luo giu')
+
+INSERT [dbo].[Delitos] ([IdCategoria], [Id_Locacion], [Fecha], [Hora], [Descripcion], [Modo]) 
+VALUES (18,18, CAST(N'2019-07-13' AS Date), CAST(N'19:56:15.8170000' AS Time), N'Un hombre se llevo mi motor', N'Persona')
+
+INSERT [dbo].[Locaciones] ([IdUsuario], [Longitud], [Latitud]) 
+VALUES (18, CAST(18.495536000 AS Decimal(9, 6)), CAST( -69.967537000 AS Decimal(9, 6)))
+
